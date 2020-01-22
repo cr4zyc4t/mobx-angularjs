74623ef1355fb1d25a4a128d6078b44224de01f5
ToanVQ <hunter9xbn@gmail.com>
2020-01-22 19:47:52 +0700

    update build tool

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es6": true,
+		"node": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"@cr4zyc4t/common",
+		"plugin:angular/johnpapa"
+	],
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parser": "babel-eslint",
+	"parserOptions": {
+		"ecmaVersion": 2018,
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"angular/file-name": "off",
+		"no-console": "off",
+	}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,70 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const outputDir = path.join(__dirname, "build");
 const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-	entry: path.join(__dirname, 'src/main.js'),
+	entry: path.join(__dirname, "src/main.js"),
 	output: {
 		path: outputDir,
-		filename: 'static/js/[name].bundle.[hash:8].js',
+		filename: "static/js/[name].bundle.[hash:8].js",
 	},
 	mode: isProduction ? "production" : "development",
+	devtool: isProduction ? "source-map" : false,
+	optimization: {
+		minimize: isProduction,
+		minimizer: [
+			new TerserJSPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: true,
+				extractComments: false,
+				terserOptions: {
+					parse: {
+						// We want terser to parse ecma 8 code. However, we don't want it
+						// to apply any minification steps that turns valid ecma 5 code
+						// into invalid ecma 5 code. This is why the 'compress' and 'output'
+						// sections only apply transformations that are ecma 5 safe
+						// https://github.com/facebook/create-react-app/pull/4234
+						ecma: 8,
+					},
+					compress: {
+						ecma: 5,
+						warnings: false,
+						// Disabled because of an issue with Uglify breaking seemingly valid code:
+						// https://github.com/facebook/create-react-app/issues/2376
+						// Pending further investigation:
+						// https://github.com/mishoo/UglifyJS2/issues/2011
+						comparisons: false,
+						// Disabled because of an issue with Terser breaking valid code:
+						// https://github.com/facebook/create-react-app/issues/5250
+						// Pending further investigation:
+						// https://github.com/terser-js/terser/issues/120
+						inline: 2,
+					},
+					mangle: {
+						safari10: true,
+					},
+					// Added for profiling in devtools
+					output: {
+						ecma: 5,
+						comments: false,
+						// Turned on because emoji and regex is not minified properly using default
+						// https://github.com/facebook/create-react-app/issues/2488
+						ascii_only: true,
+					},
+				},
+			}),
+			new OptimizeCSSAssetsPlugin({}),
+		],
+	},
 	resolve: {
-		modules: [path.resolve(__dirname, 'src'), 'node_modules']
+		modules: [path.resolve(__dirname, "src"), "node_modules"],
 	},
 	module: {
 		rules: [
@@ -28,41 +78,34 @@ module.exports = {
 						test: /\.(js|jsx|mjs)$/,
 						exclude: /node_modules/,
 						use: {
-							loader: 'babel-loader',
+							loader: "babel-loader",
 							options: {
 								cacheDirectory: !isProduction,
 								cacheCompression: true,
-							}
-						}
+								compact: isProduction,
+							},
+						},
 					},
 					{
-						test: /\.css$/,
+						test: /\.(s)css$/,
 						use: [
-							!isProduction && 'style-loader',
+							!isProduction && "style-loader",
 							isProduction && {
 								loader: MiniCssExtractPlugin.loader,
 								options: {
 									esModule: true,
 								},
 							},
-							'css-loader',
-							{
-								loader: 'sass-loader',
-								options: {
-									implementation: require('sass'),
-									sassOptions: {
-										fiber: true,
-									},
-								},
-							},
+							"css-loader",
+							"sass-loader",
 						].filter(Boolean),
 					},
 					{
 						test: /\.(png|svg|jpg|gif)$/,
 						use: {
-							loader: 'file-loader',
+							loader: "file-loader",
 							options: {
-								name: 'static/img/[name].[hash:8].[ext]',
+								name: "static/img/[name].[hash:8].[ext]",
 							},
 						},
 
@@ -70,9 +113,9 @@ module.exports = {
 					{
 						test: /\.(woff|woff2|eot|ttf|otf)$/,
 						use: {
-							loader: 'file-loader',
+							loader: "file-loader",
 							options: {
-								name: 'static/font/[name].[hash:8].[ext]',
+								name: "static/font/[name].[hash:8].[ext]",
 							},
 						},
 					},
@@ -80,21 +123,21 @@ module.exports = {
 					{
 						exclude: [/\.(js|jsx)$/, /\.html$/, /\.json$/],
 						use: {
-							loader: 'file-loader',
+							loader: "file-loader",
 							options: {
-								name: 'static/other/[name].[hash:8].[ext]',
+								name: "static/other/[name].[hash:8].[ext]",
 							},
 						},
 					},
-				]
-			}
-		]
+				],
+			},
+		],
 	},
 	plugins: [
 		isProduction && new CleanWebpackPlugin(),
 		isProduction && new MiniCssExtractPlugin({
-			filename: 'static/css/[name].[hash:8].css',
-			chunkFilename: 'static/css/[id].[hash:8].css',
+			filename: "static/css/[name].[hash:8].css",
+			chunkFilename: "static/css/[id].[hash:8].css",
 		}),
 		new HtmlWebpackPlugin({
 			template: path.join(__dirname, "public/index.html"),
@@ -119,6 +162,6 @@ module.exports = {
 	devServer: {
 		contentBase: outputDir,
 		compress: true,
-		port: 9000
-	}
-}
+		port: 9000,
+	},
+};
